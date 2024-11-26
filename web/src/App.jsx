4ee5c3bb42d4d937bcb1b2c46a185f476df04efc
DaviAravechia <alferdavi@gmail.com>
2024-11-26 11:18:56 -0300

    alteração da tela de exibição

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginForm from './components/LoginForm'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <LoginForm/>
+        <Navbar/>
       </div>
     </>
   )
